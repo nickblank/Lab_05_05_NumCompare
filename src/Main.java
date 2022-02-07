7e73fc6e472230d535d3cef5de378006156a454c
nickblank <98602112+nickblank@users.noreply.github.com>
2022-02-07 10:29:06 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+	    int numOne = 234;
+        int numTwo = 92;
+
+        if(numOne == numTwo)
+        {
+            System.out.println("Both numbers are equal");
+        }
+        else
+        {
+            System.out.println(numOne + " is not equal to " + numTwo + ".");
+        }
+    }
+}
